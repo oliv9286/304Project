a90e8abe63b6fe43fb9f8a59e842c0fe8d1b03be
Olive <olivia_ny_zhang@hotmail.com>
2012-04-01 14:01:03 -0700

    Update Employee.php

--- Employee.php
+++ Employee.php
@@ -1,4 +1,3 @@
-
 <html>
 
 <head>
@@ -15,7 +14,7 @@
 
 <?php
    $success = true;
-    $db_conn = OCILogon("ora_m4s7", "a44406106", "ug");
+    $db_conn = OCILogon("ora_y4u7", "a44229102", "ug");
     
     function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
     //echo "<br>running ".$cmdstr."<br>";
